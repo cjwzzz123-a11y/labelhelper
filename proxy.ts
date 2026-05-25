3572496a4fc44721c30dbef924a47c8ee59adbae
嘉伟 <jiawei@MacBook-Air-4.local>
2026-05-25 22:38:06 +0800

    Clean alternate headers on SEO slug pages

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,16 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 const intlMiddleware = createMiddleware(routing);
+const seoSlugPattern = /^\/(?:[a-z]{2}\/)?[a-z0-9][a-z0-9-]*$/;
 
 export function proxy(request: NextRequest) {
-  return intlMiddleware(request);
+  const response = intlMiddleware(request);
+
+  if (seoSlugPattern.test(request.nextUrl.pathname)) {
+    response.headers.delete("link");
+  }
+
+  return response;
 }
 
 export const config = {
